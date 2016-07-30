81499702f036915222befd5248a95e83920f01b5
Remco Haszing <remcohaszing@gmail.com>
2016-07-30 13:55:04 +0200

    chore: bump eslint to 3.2.0
    The new rule `no-tabs` has been added to the config.

--- index.js
+++ index.js
@@ -277,6 +277,7 @@ const rules = {
   'no-plusplus': 0,
   'no-restricted-syntax': 0,
   'no-spaced-func': 2,
+  'no-tabs': 2,
   'no-ternary': 0,
   'no-trailing-spaces': 2,
   'no-underscore-dangle': 0,
@@ -361,4 +362,4 @@ const rules = {
 
 module.exports = {
   rules
-}
+};
