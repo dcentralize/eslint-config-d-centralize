336c95ce7c168ab5453649f0320f523e84d90acb
Remco Haszing <remcohaszing@gmail.com>
2016-07-30 14:15:47 +0200

    feat(es6): add es6 rule configuration

--- es6.js
+++ es6.js
@@ -15,7 +15,68 @@ const parserOptions = {
 
 const rules = Object.assign({
   // http://eslint.org/docs/rules/#ecmascript-6
-  // Yet to be determined.
+  'arrow-body-style': [
+    2,
+    'as-needed'
+  ],
+  'arrow-parens': [
+    2,
+    'as-needed'
+  ],
+  'arrow-spacing': [
+    2,
+    {
+      before: true,
+      after: true
+    }
+  ],
+  'constructor-super': 0,
+  'generator-star-spacing': [
+    2,
+    {
+      before: true,
+      after: false
+    }
+  ],
+  'no-class-assign': 2,
+  'no-confusing-arrow': 0,
+  'no-const-assign': 2,
+  'no-dupe-class-members': 2,
+  'no-duplicate-imports': 2,
+  'no-new-symbol': 2,
+  'no-restricted-imports': 0,
+  'no-this-before-super': 2,
+  'no-useless-computed-key': 2,
+  'no-useless-constructor': 2,
+  'no-useless-rename': 2,
+  'no-var': 2,
+  'object-shorthand': [
+    2,
+    'always'
+  ],
+  'prefer-arrow-callback': 2,
+  'prefer-const': 2,
+  'prefer-reflect': 2,
+  'prefer-rest-params': 2,
+  'prefer-spread': 2,
+  'prefer-template': 0,
+  'require-yield': 2,
+  'rest-spread-spacing': [
+    2,
+    'never'
+  ],
+  'sort-imports': 2,
+  'template-curly-spacing': [
+    2,
+    'never'
+  ],
+  'yield-star-spacing': [
+    2,
+    {
+      before: true,
+      after: false
+    }
+  ]
 }, common.rules);
 
 
